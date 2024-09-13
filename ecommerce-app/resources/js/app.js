456f50c729ec200fe2bfafed8962afdc4779652c
tuan-bach123 <pham.tuan.bach@sun-asterisk.com>
2024-09-13 11:57:50 +0700

    First commit

--- ecommerce-app/resources/js/app.js
+++ ecommerce-app/resources/js/app.js
@@ -6,3 +6,17 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+function sidebarOpen() {
+    document.getElementsByClassName('cart__sidebar')[0].classList.add('show');
+    document.getElementsByClassName('cart__sidecontainer')[0].classList.add('show');
+}
+
+function sidebarClose() {
+    document.getElementsByClassName('cart__sidebar')[0].classList.remove('show');
+    document.getElementsByClassName('cart__sidecontainer')[0].classList.remove('show');
+}
+
+window.sidebarOpen = sidebarOpen;
+window.sidebarClose = sidebarClose;
+
